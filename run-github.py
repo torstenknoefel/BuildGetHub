7a2fcc37cb7c11c26cc0ef233dffecd539157946
torstenknoefel <torsten.knoefel@googlemail.com>
2020-02-12 18:47:51 +0000

    final version - added pip3 install -r requirements.txt

--- run-github.py
+++ run-github.py
@@ -7,7 +7,7 @@ import time, datetime
 #from collections import deque
 #from git import Repo
 #import tarfile
-from git import Repo
+from git import Repo, RemoteProgress
 
 
 def log(log_text):
@@ -15,7 +15,7 @@ def log(log_text):
 
 
 def error(log_text):
-    log('ERROR: ', log_text)
+    log('ERROR: ' + log_text)
     sys.exit(1)
 
 
@@ -31,22 +31,28 @@ log('Starting installation from GITHUB')
 REPO_URL = os.getenv('REPO_URL', '')
 if REPO_URL == '':
     error('No repository specified - You need to set the environment variable REPO_URL.')
-log('GitHub repository: ', REPO_URL)
+log('GitHub repository: %s' % REPO_URL)
 GIT_USERNAME = os.getenv('GIT_USERNAME', '')
 GIT_PASSWORD = os.getenv('GIT_PASSWORD', '')
 # GIT_USERNAME empty XOR GIT_PASSWORD empty
 if (GIT_USERNAME == '' or GIT_PASSWORD == '') and not (GIT_USERNAME == '' and GIT_PASSWORD == ''):
     error('You need to specify either both of GIT_USERNAME and GIT_PASSWORD or NONE.')
-BASE_DIR = os.getenv('BASE_DIR', '/')
+BASE_DIR = os.getenv('BASE_DIR', '~/repo')
 REPO_BRANCH = os.getenv('REPO_BRANCH', 'master')
 log('Branch: %s' % REPO_BRANCH)
 
-# make sure git cgets the credentials
+# make sure git gets the credentials
 act_dir = os.path.dirname(os.path.abspath(__file__))
+log(act_dir)
 os.environ['GIT_ASKPASS'] = os.path.join(act_dir, 'askpass.py')
 
 # clone REPO
+BASE_DIR = os.path.expanduser(BASE_DIR)
+os.mkdir(BASE_DIR)
 log('Cloning into %s' % BASE_DIR)
+
 Repo.clone_from(REPO_URL, BASE_DIR, branch=REPO_BRANCH, progress=CloneProgress())
 
-time.sleep(3600)
+log('Starting run-me.py')
+os.system('pip3 install -r '+BASE_DIR+'/requirements.txt')
+os.system('python3 '+BASE_DIR+'/run-me.py')
